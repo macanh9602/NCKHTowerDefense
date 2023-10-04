64060ca3ffdc3dc8fc2844e33f8c0630faac5ae4
ducanh0906 <ducanhmac113@gmail.com>
2023-10-05 00:14:52 +0700

    update

--- Assets/Script/BuildingTypeSO.cs
+++ Assets/Script/BuildingTypeSO.cs
@@ -1,21 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "SO/BuildingTypeSO")] 
-public class BuildingTypeSO : ScriptableObject
+[CreateAssetMenu(menuName = "SO/BuildingTypeSO")]
+public abstract class BuildingTypeSO : ScriptableObject
 {
     public string nameBuilding;
     public Transform[] pref;
     public int level;
     public int costToBuildOrUpgrate;
     public int health;
-    
+
+    //    public List<DefenderBuilding> defenders = new List<DefenderBuilding>();
+    //    public List<FarmBuilding> farm = new List<FarmBuilding>();
+    //}
 }
-public class DefenderBuilding
-{
-    public int damage;
-    public int armor;
-    public int speedATK;
-    public int radiusATK;
-}
+//[Serializable]
+//public class FarmBuilding
+//{
+//    public int adsaf;
+//    public int adaf;
+//}
+//[Serializable]
+//public class DefenderBuilding
+//{
+//    public int damage;
+//    public int armor;
+//    public int speedATK;
+//    public int radiusATK;
+//}
+
---
+++ Assets/Script/TowerDefenseSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/buildingType/TowerDefense")]
+public class TowerDefenseSO : BuildingTypeSO
+{
+    public int atk;
+    public int armor;
+    public int radiusATK;
+}
